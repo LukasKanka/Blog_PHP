8ae34b289228dd942e486ec0640b63b53cb6ae26
LukasKanka <lukas.kanka@outlook.cz>
2024-09-05 14:01:22 +0000

    test cloud ide 2

---
+++ public/project/index.php
@@ -0,0 +1,56 @@
+</html>
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css"
+        integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous" />
+    <title>Odkazy a mé repozitáře</title>
+</head>
+
+<body>
+    <h1>Omlouváme se, stránka se připravuje</h1>
+    <p><a href='../index.php'>Zpět na hlavní stranu</a></p>
+    <h2>Mé Git ropossitáře:</h2>
+    <ul>
+        <li> <a href="https://git.archoslinux.cz/kankys" target="_blank"><i class="fa fa-gitea"></i> Gitea na ArchLinux
+                CZ</a></li>
+        <li><a href="https://git.archoslinux.cz/kankys" target="_blank"><i class="fa fa-gitea"></i> Gitea -
+                archoslinux</a></li>
+        <li><a href="https://git.arch-linux.cz/kankys" target="_blank"><i class="fa fa-gitea"></i> Gitea -
+                arch-linux</a></li>
+        <li><a href="https://codeberg.org/Kankys" target="_blank"><i class="fa fa-gitea"></i> Codeberg</a></li>
+        <li><a href="https://gitlab.com/LukasKanka" target="_blank"><i class="fa fa-gitlab"></i> GitLab</a></li>
+        <li><a href="https://github.com/LukasKanka" target="_blank"><i class="fa fa-github"></i> GitHub</a></li>
+    </ul>
+    <h2>Mé projekty a kam přispívám:</h2>
+    <ul>
+        <li><p><a href="../butcher" target="_blank">Textová hra plná napětí z podsvětí temného města</a></p></li>
+        <li><p><a href="https://endeavouros.cz/" target="_blank">Jsem zakládající člen České komunity EndeavourOS</a></p></li>
+    </ul>
+    <h2>Sociální sítě kde mě najdete:</h2>
+    <ul>
+        <li><a href="https://mastodon.arch-linux.cz/deck/@Kankys" target="_blank"><i class="fa fa-mastodon"></i> Mastodon</a></li>
+        <li><a href="https://www.linkedin.com/in/luk%C3%A1%C5%A1-ka%C5%88ka-b2a0a1a0/" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+        <li><a href="https://t.me/Kankys" target="_blank"><i class="fa fa-telegram"></i> Telegram</a></li>
+        <li><a href="https://pixelfed.cz/i/web/profile/464150575238569182"><i class="fa fa-pixelfed"></i> Pixelded CZ</a></li>
+        <li><a href="https://discord.com/kankys_" target="_blank"><i class="fa fa-discord"></i> Discord</a></li>
+    </ul>
+    <h2>OpenSource a můj oblíbený software:</h2>
+    <ul>
+        <li><p><a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank"><i class="fa fa-firefox"></i> Firefox</a> - nejoblíbenější, nestarnoucí webový prohlížeč.</p></li>
+        <li><p><a href="https://vscodium.com/" target="_blank"><i class="fa fa-vscodium"></i> VSCodium</a> - opensource alternativa k VSCode bez Microsoft telemetrie.</p></li>
+        <li><p><a href="https://filezilla-project.org/" target="_blank"><i class="fa fa-filezilla"></i> FileZilla</a> - FTP klient který zapínám hned po startu pc jako první.</p></li>
+        <li><p><a href="https://www.apachefriends.org/index.html" target="_blank"><i class="fa fa-xampp"></i> XAMPP</a> - vývojové prostředí pro PHP.</p></li>
+        <li><p><a href="https://netbeans.apache.org/front/main/index.html" terget="_blank"><i class="fa fa-netbeans"></i> NetBeans</a> - tuto stálici na poli IDE teprve testuji.</p></li>
+    </ul>
+    <h2>Claudové služby</h2>
+    <ul>
+        <li>Codeanywhere</li>
+    </ul>
+    <p>Test FTP online Cloud IDE</p>
+</body>
+
+</html>
